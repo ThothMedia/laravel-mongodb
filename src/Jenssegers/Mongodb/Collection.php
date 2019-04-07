fbf6ba7ab5db32faacc1445eb2183c9ae5629cd5
iPol2n <izporn@gmail.com>
2019-04-07 22:09:31 +0700

    Update to support aggregate with cursor for MongoDB 3.6

--- src/Jenssegers/Mongodb/Collection.php
+++ src/Jenssegers/Mongodb/Collection.php
@@ -56,7 +56,15 @@ class Collection {
 
         $start = microtime(true);
 
-        $result = call_user_func_array(array($this->collection, $method), $parameters);
+        if($method == 'aggregate'){
+            $result = call_user_func_array(array($this->collection, $method), [$parameters, ['cursor' =>(object) []]]);
+        } else {
+            $result = call_user_func_array(array($this->collection, $method), $parameters);
+        }
+
+        if($method == 'aggregate'){
+            $result['result'] = $result['cursor']['firstBatch'];
+        }
 
         // Once we have run the query we will calculate the time that it took to run and
         // then log the query, bindings, and execution time so we will report them on
